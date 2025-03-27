936114682a7a1463150e651994496e04e7fef510
Mikhail8815 <mishutka150588@mail.ru>
2025-03-27 23:37:54 +0300

    Week_01_Clock

---
+++ src/s2-homeworks/hw03/HW3.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react'
+import { v1 } from 'uuid'
+import s2 from '../../s1-main/App.module.css'
+import GreetingContainer from './GreetingContainer'
+
+/*
+* 1 - описать тип UserType СДЕЛАЛ
+* 2 - указать нужный тип в useState с users СДЕЛАЛ
+* 3 - дописать типы и логику функции pureAddUserCallback и проверить её тестами СДЕЛАЛ
+* 4 - в файле GreetingContainer.tsx дописать типизацию пропсов СДЕЛАЛ
+* 5 - в файле GreetingContainer.tsx указать нужные типы в useState с name и error СДЕЛАЛ
+* 6 - в файле GreetingContainer.tsx дописать тип и логику функции setNameCallback СДЕЛАЛ
+* 7 - в файле GreetingContainer.tsx дописать логику функций pureAddUser, pureOnBlur, pureOnEnter и проверить их тестами СДЕЛАЛ
+* 8 - в файле GreetingContainer.tsx вычислить количество добавленных и имя последнего (totalUsers, lastUserName) СДЕЛАЛ
+* 9 - в файле Greeting.tsx дописать типизацию пропсов СДЕЛАЛ
+* 10 - в файле Greeting.tsx вычислить inputClass в зависимости от наличия ошибки
+* 11 - сделать стили в соответствии с дизайном
+* */
+
+// types
+export type UserType = {
+    _id: string // need to fix any
+    name: string // need to fix any
+}
+
+export const pureAddUserCallback = (name: string, setUsers: (users: UserType[])=>void, users: UserType[]) => { // need to fix any
+    const user = {
+        _id: v1(),
+        name: name// need to fix
+    }
+    setUsers([...users, user])
+}
+
+const HW3 = () => {
+    const [users, setUsers] = useState<UserType[]>([]) // need to fix any
+
+    const addUserCallback = (name: any) => { // need to fix any
+        pureAddUserCallback(name, setUsers, users)
+    }
+
+    return (
+        <div id={'hw3'}>
+            <div className={s2.hwTitle}>Homework #3</div>
+            {/*для автоматической проверки дз (не менять)*/}
+
+            <div className={s2.hw}>
+                <GreetingContainer
+                    users={users}
+                    addUserCallback={addUserCallback}
+                />
+            </div>
+        </div>
+    )
+}
+
+export default HW3
---
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -0,0 +1,102 @@
+import React, {useEffect, useState} from 'react'
+import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
+import {restoreState} from '../hw06/localStorage/localStorage'
+import s from './Clock.module.css'
+
+function Clock() {
+    const [timerId, setTimerId] = useState<any>(undefined)
+    // for autotests // не менять // можно подсунуть в локалСторэдж нужную дату, чтоб увидеть как она отображается
+    const [date, setDate] = useState<Date>(new Date(restoreState('hw9-date', Date.now())))
+    const [show, setShow] = useState<boolean>(false)
+
+    const start = () => {
+        if (timerId === undefined) {
+            const id = setInterval(() => {
+                setDate(new Date());
+            }, 1000);
+            setTimerId(id);
+            //
+        }
+        // пишут студенты // запустить часы (должно отображаться реальное время, а не +1)
+        // сохранить ид таймера (https://learn.javascript.ru/settimeout-setinterval#setinterval)
+
+    }
+
+    const stop = () => {
+        if (timerId !== undefined) {
+            clearInterval(timerId);
+            setTimerId(undefined);
+        }
+        // пишут студенты // поставить часы на паузу, обнулить ид таймера (timerId <- undefined)
+    }
+
+    const onMouseEnter = () => {
+        setShow(true)// пишут студенты // показать дату если наведена мышка
+    }
+    const onMouseLeave = () => { // пишут студенты // спрятать дату если мышка не наведена
+        setShow(false)
+    }
+
+    const stringTime = date.toLocaleTimeString('en-GB', { hour: '2-digit', minute: '2-digit', second: '2-digit' });
+    const stringDate = date.toLocaleDateString('en-GB', { day: '2-digit', month: '2-digit', year: 'numeric' }).replace(/\//g, '.');
+    const stringDay = date.toLocaleString('en-US', { weekday: 'long' });
+    const stringMonth = date.toLocaleString('en-US', { month: 'long' });
+
+    // const stringTime = 'date->time' || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
+    // const stringDate = 'date->date' || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+    //
+    // // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
+    // const stringDay = 'date->day' || <br/> // пишут студенты
+    // const stringMonth = 'date->month' || <br/> // пишут студенты
+    //
+
+    return (
+        <div className={s.clock}>
+            <div
+                id={'hw9-watch'}
+                className={s.watch}
+                onMouseEnter={onMouseEnter}
+                onMouseLeave={onMouseLeave}
+            >
+                <span id={'hw9-day'}>{stringDay}</span>,{' '}
+                <span id={'hw9-time'}>
+                    <strong>{stringTime}</strong>
+                </span>
+            </div>
+
+            <div id={'hw9-more'}>
+                <div className={s.more}>
+                    {show ? (
+                        <>
+                            <span id={'hw9-month'}>{stringMonth}</span>,{' '}
+                            <span id={'hw9-date'}>{stringDate}</span>
+                        </>
+                    ) : (
+                        <>
+                            <br/>
+                        </>
+                    )}
+                </div>
+            </div>
+
+            <div className={s.buttonsContainer}>
+                <SuperButton
+                    id={'hw9-button-start'}
+                    disabled={timerId !== undefined} // пишут студенты // задизэйблить если таймер запущен
+                    onClick={start}
+                >
+                    start
+                </SuperButton>
+                <SuperButton
+                    id={'hw9-button-stop'}
+                    disabled={timerId === undefined} // пишут студенты // задизэйблить если таймер не запущен
+                    onClick={stop}
+                >
+                    stop
+                </SuperButton>
+            </div>
+        </div>
+    )
+}
+
+export default Clock
