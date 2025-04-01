7e36a5393873b464ea172bbf5e7f4650391f5648
Mikhail8815 <mishutka150588@mail.ru>
2025-04-01 23:18:34 +0300

    SP_04_WEEK_04 (Theme)

--- src/s2-homeworks/hw07/common/c5-SuperSelect/SuperSelect.tsx
+++ src/s2-homeworks/hw07/common/c5-SuperSelect/SuperSelect.tsx
@@ -37,8 +37,9 @@ const SuperSelect: React.FC<SuperSelectPropsType> = ({
 
     const onChangeCallback = (e: ChangeEvent<HTMLSelectElement>) => {
         if (onChangeOption) {
-            onChangeOption(e.currentTarget.value)
+            onChangeOption(Number(e.currentTarget.value))
         }
+
         // делают студенты
     }
 
